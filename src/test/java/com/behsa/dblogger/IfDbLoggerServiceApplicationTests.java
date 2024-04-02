976cdc68a05fb54368f75100446475e3ca1be416
n.zare <n.zare@behsacorp.com>
2024-04-02 17:00:35 +0330

    initial commit

---
+++ src/test/java/com/behsa/dblogger/IfDbLoggerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.behsa.dblogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfDbLoggerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
